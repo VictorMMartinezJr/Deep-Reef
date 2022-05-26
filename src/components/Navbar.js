49b0d3cd9a38ca725a05fdfd4804b5d93cb7f91f
Victor Martinez Jr <victormtzcodes@gmail.com>
2022-05-26 15:05:47 -0400

    added responsiveness to hero section and navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,16 @@ import jellyfish from "../assets/jellyfish.svg";
 
 const Navbar = ({ navActive, setNavActive }) => {
   return (
-    <nav className="h-[10vh] w-full text-gray-200  flex justify-between items-center relative">
+    <nav className="h-[10vh] w-full text-gray-200 flex justify-between items-center relative">
       {/* Company Logo & Name */}
-      <div className="ml-8 flex items-center z-10">
+      <div className="ml-8 flex items-center z-10 xl:ml-16">
         <img src={jellyfish} alt="" className="h-8" />
         <h2 className="ml-3 font-medium tracking-wide text-lg">DEEP REEF</h2>
       </div>
 
       {/* Menu Burger */}
       <button
-        className="burger mr-8 bg-blue-500 p-3 rounded-full grid place-items-center cursor-pointer z-[3]"
+        className="burger mr-8 bg-blue-500 p-3 rounded-full grid place-items-center cursor-pointer lg:hidden"
         onClick={() => setNavActive(!navActive)}
       >
         {/* Line 1 */}
@@ -39,24 +39,25 @@ const Navbar = ({ navActive, setNavActive }) => {
           }`}
         ></div>
       </button>
+
       {/* Links */}
       <ul
         className={`nav-links flex flex-col items-end absolute right-0 top-[100%] w-[100%] bg-gray-900 bg-opacity-75 backdrop-blur-xl z-[2] ${
           navActive
             ? "transform translate-x-0 transition-transform duration-300"
             : "transform translate-x-[100%] transition-transform duration-300"
-        }`}
+        } lg:static lg:flex-row lg:bg-none lg:bg-opacity-0 lg:backdrop-blur-0 lg:translate-x-0 lg:items-center lg:w-auto xl:mr-16`}
       >
-        <li className=" text-xl font-bold my-4 py-4 mr-8 cursor-pointer">
+        <li className=" text-xl font-bold my-4 py-4 mr-8 cursor-pointer lg:text-base">
           How it works?
         </li>
-        <li className=" text-xl font-bold my-4 py-4 mr-8 cursor-pointer">
+        <li className=" text-xl font-bold my-4 py-4 mr-8 cursor-pointer lg:text-base">
           Roadmap and tokenomics
         </li>
-        <li className=" text-xl font-bold my-4 py-4 mr-8 cursor-pointer">
+        <li className=" text-xl font-bold my-4 py-4 mr-8 cursor-pointer lg:text-base">
           Charity
         </li>
-        <li className=" text-xl font-bold my-4 py-4 mr-8 cursor-pointer">
+        <li className=" text-xl font-bold my-4 py-4 mr-8 cursor-pointer lg:text-base">
           How to buy?
         </li>
       </ul>
